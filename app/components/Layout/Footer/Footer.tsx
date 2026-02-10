8c27b255c4ccca2be772b43471181e1b343a850b
Remo Lalata <remo@winkreports.com>
2026-02-10 21:28:17 +0800

    style: wrap footer content in container

--- app/components/Layout/Footer/Footer.tsx
+++ app/components/Layout/Footer/Footer.tsx
@@ -5,25 +5,27 @@ export const Footer = () => {
 
   return (
     <footer className='p-5 text-center lg:text-left'>
-      <p>
-        <span>{t('designBy')} </span>
-        <a
-          href='https://www.figma.com/community/file/893705420616737018'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Emmanuel García
-        </a>
-        <span>. {t('licensedUnder')} </span>
-        <a
-          href='https://creativecommons.org/licenses/by/4.0/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          CC BY 4.0
-        </a>
-        <span>, {t('viaFigma')}</span>
-      </p>
+      <div className='mx-auto container'>
+        <p>
+          <span>{t('designBy')} </span>
+          <a
+            href='https://www.figma.com/community/file/893705420616737018'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Emmanuel García
+          </a>
+          <span>. {t('licensedUnder')} </span>
+          <a
+            href='https://creativecommons.org/licenses/by/4.0/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            CC BY 4.0
+          </a>
+          <span>, {t('viaFigma')}</span>
+        </p>
+      </div>
     </footer>
   );
 };
